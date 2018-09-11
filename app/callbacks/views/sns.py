ca1159cebf407b554befe56afddfbaa1ea4e14a5
Robert Scott <code@humanleg.org.uk>
2018-09-11 11:54:51 +0100

    digitalmarketplace-utils dependency: bump to v44.0.0

--- app/callbacks/views/sns.py
+++ app/callbacks/views/sns.py
@@ -398,7 +398,7 @@ def handle_s3_sns():
                 try:
                     notify_client.send_email(
                         current_app.config["DM_DEVELOPER_VIRUS_ALERT_EMAIL"],
-                        template_id=current_app.config["NOTIFY_TEMPLATES"]["developer_virus_alert"],
+                        template_name="developer_virus_alert",
                         personalisation={
                             "region_name": record["awsRegion"],
                             "bucket_name": s3_bucket_name,
--- tests/callbacks/views/test_sns.py
+++ tests/callbacks/views/test_sns.py
@@ -826,7 +826,7 @@ class TestHandleS3Sns(BaseCallbackApplicationTest):
                             "region_name": "howth-west-2",
                             "sns_message_id": "424344def",
                         },
-                        template_id=mock.ANY,
+                        template_name="developer_virus_alert",
                     ),
                 ),
                 # expected_tagset
