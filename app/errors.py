fcc57ddbeef2c18d871292441705afd71e061340
Robert Scott <code@humanleg.org.uk>
2018-09-17 11:59:16 +0100

    generic_error_handler: this also has to be able to cope with non HTTPExceptions being raised
    ...in which case it should return a simple 500 response.

--- app/errors.py
+++ app/errors.py
@@ -17,13 +17,18 @@ def validation_error(e):
 
 
 def generic_error_handler(e):
-    # for the most part, the default HTTPExceptions render themselves in the desired way if returned as a response. the
-    # only change we want to make is to enclose the error description in json
-    response = e.get_response()
-    response.set_data(json.dumps({"error": getattr(e, "description", "Internal error")}))
-    response.mimetype = current_app.config["JSONIFY_MIMETYPE"]
+    try:
+        # initially we'll try and assume this is an HTTPException of some sort.  for the most part, the default
+        # HTTPExceptions render themselves in the desired way if returned as a response. the only change we want to
+        # make is to enclose the error description in json.
+        response = e.get_response()
+        response.set_data(json.dumps({"error": e.description}))
+        response.mimetype = current_app.config["JSONIFY_MIMETYPE"]
 
-    return response
+        return response
+    except Exception:
+        # either e wasn't an HTTPException or something went wrong in trying to jsonify it
+        return jsonify(error="Internal error"), 500
 
 
 for code in range(400, 599):
